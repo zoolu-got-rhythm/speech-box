f55917aab64f0f517ea05704882606eeb7c5c6ed
christopher morris <slurp@v460-0aa44ce3.wifi.cf.ac.uk>
2019-04-01 01:38:12 +0100

    strange javaScript language bug discovered with -= with 170 / 6. cannot reach 0 which is strange and buggy behaviour

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function plotDialogueBoxCoords(originPoint, width, height, borderRadius, spacesB
 
     // draw from top-left to top-right along x axis
 
-    let widthMinusBorderRadius = (width - borderRadius * 2);
+    const widthMinusBorderRadius = (width - borderRadius * 2);
     let xDistance = widthMinusBorderRadius / spacesBetweenPoints;
 
     for(var xLocation = 0; xLocation < widthMinusBorderRadius; xLocation += xDistance){
@@ -34,13 +34,13 @@ function plotDialogueBoxCoords(originPoint, width, height, borderRadius, spacesB
 
     if(xLocation == widthMinusBorderRadius){
         lastPoint = new Point(originPoint.x + xLocation, originPoint.y);
-        console.log("hit 1")
+        console.log("hit 1");
     }
 
     let topRightCurve = plotAngleCurvCoords(
         new Point(lastPoint.x, lastPoint.y + borderRadius),
         borderRadius,
-        6,
+        4,
         CURVE_ANGLE_ENUM.TOP_RIGHT);
 
     pointsArray.push(...topRightCurve);
@@ -50,7 +50,7 @@ function plotDialogueBoxCoords(originPoint, width, height, borderRadius, spacesB
     // draw from top-right to bottom-right along y axis
 
     let heightMinusBorderRadius = height - (borderRadius * 2);
-    let yDistance = heightMinusBorderRadius / (spacesBetweenPoints);
+    let yDistance = heightMinusBorderRadius / (spacesBetweenPoints / 2);
 
     for(var yLocation = 0; yLocation < heightMinusBorderRadius; yLocation += yDistance){
         pointsArray.push(new Point(originPoint.x + widthMinusBorderRadius + borderRadius, originPoint.y + yLocation + borderRadius ));
@@ -70,12 +70,44 @@ function plotDialogueBoxCoords(originPoint, width, height, borderRadius, spacesB
     let bottomRightCurve = plotAngleCurvCoords(
         new Point(lastPoint.x - borderRadius, lastPoint.y),
         borderRadius,
-        6,
+        4,
         CURVE_ANGLE_ENUM.BOTTOM_RIGHT);
 
     pointsArray.push(...bottomRightCurve);
 
     // for()
+
+
+    // draw from bottom-right to bottom-left along x axis
+    console.log(widthMinusBorderRadius);
+    console.log("width minus border rad");
+    xDistance = widthMinusBorderRadius / spacesBetweenPoints;
+
+    // need to fix this
+    for(let i = spacesBetweenPoints; i > 0; i--){
+        pointsArray.push(new Point(originPoint.x + (i * xDistance), originPoint.y + height));
+        console.log(xLocationBottom);
+        console.log("in loop");
+    }
+    console.log(xLocationBottom);
+
+    // pointsArray.push(new Point(originPoint.x + xLocationBottom, originPoint.y));
+
+
+    // if(xLocationBottom == 0){
+        lastPoint = new Point(originPoint.x + xLocationBottom, originPoint.y);
+    //     console.log("hit 3");
+    // }
+
+    console.log(xLocationBottom);
+
+    let bottomLeftCurve = plotAngleCurvCoords(
+        new Point(lastPoint.x + borderRadius * 2, lastPoint.y + height - borderRadius),
+        borderRadius,
+        4,
+        CURVE_ANGLE_ENUM.BOTTOM_LEFT);
+
+    pointsArray.push(...bottomLeftCurve);
 
 
     return pointsArray;
@@ -98,6 +130,10 @@ function plotAngleCurvCoords(centerPoint, radius, nOfPoints, curveAngleEnum){
             break;
         case CURVE_ANGLE_ENUM.BOTTOM_RIGHT:
             curveAngleDegreesToSubtract = degreesToRadians(0);
+            break;
+        case CURVE_ANGLE_ENUM.BOTTOM_LEFT:
+            curveAngleDegreesToSubtract = degreesToRadians(270);
+            break;
         default:
             // throw "curve angle not specified"; // why does this always execute?
             break;
@@ -145,6 +181,35 @@ function drawCoords(ctx, arr){
     }
 }
 
+function drawCoordsWithTimer(ctx, arr){
+    let prevCoOrd;
+    let i = 0;
+     // start
+    var timer = window.setInterval(()=>{
+        let currentPoint = arr[i];
+
+        if(i % 2 == 0){
+            ctx.strokeStyle = "lime";
+        }else{
+            ctx.strokeStyle = "pink";
+        }
+
+        if(i >= 1){
+            ctx.beginPath();
+            ctx.lineWidth = 8;
+            ctx.moveTo(prevCoOrd.x, prevCoOrd.y);// start
+            ctx.lineTo(currentPoint.x, currentPoint.y);
+            ctx.stroke();
+        }
+        prevCoOrd = currentPoint;
+        i++;
+        if(i == arr.length)
+            clearInterval(timer);
+    }, 1000 / 60);
+
+     // end
+}
+
 function Point(x, y){
     this.x = x;
     this.y = y;
@@ -152,10 +217,12 @@ function Point(x, y){
 
 let origin = new Point(200, 200);
 
-let plotArr = plotDialogueBoxCoords(origin, 200, 100, 25, 8);
+let plotArr = plotDialogueBoxCoords(origin, 300, 200, 15, 6);
 let circlePlotArr = plotAngleCurvCoords(origin, 50, 8);
 
-drawCoords(ctx, plotArr);
+// drawCoords(ctx, plotArr);
+drawCoordsWithTimer(ctx, plotArr);
+
 
 
 
